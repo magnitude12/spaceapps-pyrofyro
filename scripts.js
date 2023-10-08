497206d73230e22faa90879e1749d9c7b4ab94e9
magnitude12 <magnitude12i@gmail.com>
2023-10-08 10:58:38 +0545

    initial structure

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// JavaScript for Landing Page
+
+// Function to handle the "View Wildfire Data" button click
+document.getElementById("view-data-button").addEventListener("click", function () {
+    // Redirect to the View Page (you need to implement this)
+    window.location.href = "view/index.html";
+});
+
+// Function to handle the "Report Wildfire Data" button click
+document.getElementById("report-data-button").addEventListener("click", function () {
+    // Redirect to the Report Page (you need to implement this)
+    window.location.href = "report/index.html";
+});
